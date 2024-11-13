fbc93ceb63cbed88e6e3495333b7f14f6f8cdc95
Adrian Williams <alexis.williams0925@gmail.com>
2024-11-12 20:36:04 -0500

    Add ball

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
@@ -11,14 +11,36 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
+        // Place your variables here:
+        Color daySky = new Color(0x7f, 0xa8, 0xb5);
+        Color nightSky = new Color(0x4c, 0x54, 0x9b);
+        Color sun = new Color(0xff, 0xfd, 0x95);
+        Color moon = new Color(0xfe, 0xfe, 0xfe);
+        Color clouds = new Color(0x78, 0x78, 0x9e);
 
+        float[] xaxis = [];
+        float[] yaxis = [];
+        float pelletRadius = 10;
+        float xCursor;
+        float nightzone = 200;
+
+        Vector2 circleZone = new Vector2(300, 700);
+        int radius = 10;
+        float num1 = 0;
+        float num2 = 0;
+        Vector2 circleBrick = new Vector2(50, 50);
+        int brickRadius = 10;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Circle Break");
 
+            Window.SetSize(600, 800);
+          
+           
         }
 
         /// <summary>
@@ -26,7 +48,16 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(daySky);
 
-        }
-    }
-}
+                Draw.FillColor = Color.White;
+                Draw.Circle(circleZone, radius);
+            }
+
+           
+                }
+            }
+        
+    
+
+
